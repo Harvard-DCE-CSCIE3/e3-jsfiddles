cd2825100addcc48ac17d808803dc91b6f0f8156
Laurence Bouthillier <lbouthillier@fas.harvard.edu>
2023-09-25 17:02:05 -0700

    Update demo.js - variable declarations update

--- examples/basic-conditional-aresbyfr/demo.js
+++ examples/basic-conditional-aresbyfr/demo.js
@@ -1,6 +1,6 @@
 "use strict"
 
-var hasBananas = confirm("Do you have bananas? ([OK] for yes, [Cancel] for no)");
+const hasBananas = confirm("Do you have bananas? ([OK] for yes, [Cancel] for no)");
 
 // we'll check and write some output if hasBananas is true
 if (hasBananas){
