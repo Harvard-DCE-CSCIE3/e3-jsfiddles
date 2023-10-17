67c87c6d4a8eb15840ae527a98a5ecfe30c3b2ec
Laurence Bouthillier <lbouthillier@fas.harvard.edu>
2023-10-17 15:25:12 -0700

    Update demo.js
    updated variable scope

--- practice-sets/functions-as-arguments-gu0tmwva-r179/demo.js
+++ practice-sets/functions-as-arguments-gu0tmwva-r179/demo.js
@@ -1,11 +1,11 @@
 // Initialize our two test-case arrays
-var arrayOne = [1, 3, 5, 7, 9];
-var arrayTwo = [1, 3, 5, "seven", 9];
+const arrayOne = [1, 3, 5, 7, 9];
+const arrayTwo = [1, 3, 5, "seven", 9];
 
 // SOLUTION: isNumber returns either a number
 // or a value that's equivalent to false (such as NaN)
 // This is interpreted by every() as true or false.
-var isNumber = function (element) {
+const isNumber = function (element) {
     return Number(element);
 }
 /* THIS COULD ALSO BE DONE WITH A DELCARED FUNCTION:
