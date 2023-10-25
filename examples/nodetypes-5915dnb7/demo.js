bd97febef95af3077d63b7b1c73f9544eefc65d9
Laurence Bouthillier <lbouthillier@fas.harvard.edu>
2023-10-24 21:56:02 -0700

    Update demo.js
    updated variable declarations

--- examples/nodetypes-5915dnb7/demo.js
+++ examples/nodetypes-5915dnb7/demo.js
@@ -1,5 +1,5 @@
 // starting with 
-for (var i=0; i<document.body.childNodes.length; i++){
+for (let i=0; i<document.body.childNodes.length; i++){
     console.log("Node is: "+document.body.childNodes[i] + " is of type " + document.body.childNodes[i].nodeType);
 }
 console.log("Node is: "+document + " is of type " + document.nodeType)
