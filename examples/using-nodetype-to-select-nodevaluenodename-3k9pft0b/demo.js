4b560aaebc203ba83961187b310408b161d81f09
Laurence Bouthillier <lbouthillier@fas.harvard.edu>
2023-10-24 21:56:40 -0700

    Update demo.js
    updated variable declarations

--- examples/using-nodetype-to-select-nodevaluenodename-3k9pft0b/demo.js
+++ examples/using-nodetype-to-select-nodevaluenodename-3k9pft0b/demo.js
@@ -1,6 +1,6 @@
 // iterate over document.body's childNodes
-for (var i=0; i<document.body.childNodes.length; i++){
-    var currentNode = document.body.childNodes[i];
+for (let i=0; i<document.body.childNodes.length; i++){
+    let currentNode = document.body.childNodes[i];
     if (currentNode.nodeType == 1){
         // this is an element node
         console.log("Node is: "+currentNode.nodeName + " and is a "+currentNode);
