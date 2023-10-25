2afbbbb478dfe63f529fa5a8c41a1c46b4eb37f9
Laurence Bouthillier <lbouthillier@fas.harvard.edu>
2023-10-24 21:55:22 -0700

    Update demo.js
    updated variable declarations

--- examples/iterating-over-an-elements-child-nodes-jdqwh25u/demo.js
+++ examples/iterating-over-an-elements-child-nodes-jdqwh25u/demo.js
@@ -1,4 +1,4 @@
 // starting with 
-for (var i=0; i<document.body.childNodes.length; i++){
+for (let i=0; i<document.body.childNodes.length; i++){
     console.log("Node is: "+document.body.childNodes[i]);
 }
