aff14bd6a13940b2812d761622b43cd7c5680c91
Laurence Bouthillier <lbouthillier@fas.harvard.edu>
2024-07-10 08:57:15 -0400

    Update demo.js to merge with commented version

--- practice-sets/parameter-validation-fbd7015r/demo.js
+++ practice-sets/parameter-validation-fbd7015r/demo.js
@@ -1,15 +1,23 @@
-//for this assignment, we'll deal with input validation
-var scoresArray4 = "this isn't an array";
-var scoresArray5 = [76, 83, "90"];
-var scoresArray6 = [88, 73, "bob", 100]; 
+// the sample input data we'll be validating
 
-//  function calculateSum goes here
+var scoresArray4 = "this is not an array";  
+//hint: try testing to see if this is actually an array before doing anything else in the function.  Remember that 'typeof' works for simple data types, but [object].constructor.name will tell you the type of many objects. We show this in video 4.4 (towards the end).   
+
+var scoresArray5 = [76, 83, "90"];
+// hint: try converting each (or each non-numeric) array element to a number as you come to it
+
+var scoresArray6 = [88, 73, "bob", 100]; 
+// hint: while you're converting all array elements to numbers, use a conditional to check to see if it worked before adding the value to the sum
+
+// function calculateSum() is here
+// You will modify this function so that none of the above cases
+// "break" the code. If the sum simply can't be done (as in scoresArray4),
+// return the value 'null' or nothing at all, and write an error to the console.
+// Otherwise, if a sum can still be calculated, return that sum
 function calculateSum(arr){
-	var sum=0;
-  for (var i=0; i<arr.length; i++){
-  	sum+=arr[i];
-  }
-  return sum;
+  // your code here
+
+  	
 }
 
 document.getElementById("sum4").innerHTML = calculateSum(scoresArray4);
