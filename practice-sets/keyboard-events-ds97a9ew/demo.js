c24acb3523abeec6f19de2364eb4a613d1436c8c
Laurence Bouthillier <lbouthillier@fas.harvard.edu>
2024-07-24 16:22:33 -0400

    Update demo.js to correct scope issues

--- practice-sets/keyboard-events-ds97a9ew/demo.js
+++ practice-sets/keyboard-events-ds97a9ew/demo.js
@@ -1,29 +1,34 @@
-// Add your event handler here to listen for keyboard
-//   activity and call the functions below as needed.
+(function(){
+
+
+    // Add your event handler here to listen for keyboard
+    //   activity and call the functions below as needed.
 
 
 
-// initial location of the puck, and step size
-var step = 4;
-var pTop = 90;
-var pLeft = 190;
+    // initial location of the puck, and step size
+    let step = 4;
+    let pTop = 90;
+    let pLeft = 190;
 
-// utility functions that will move the puck
-var puck = document.getElementById("puck");
+    // utility functions that will move the puck
+    let puck = document.getElementById("puck");
 
-function mvup(){
-    pTop -= step;
-    puck.style.top = pTop + "px";
-}
-function mvdown(){
-    pTop += step;
-    puck.style.top = pTop + "px";
-}
-function mvleft(){
-    pLeft -= step;
-    puck.style.left = pLeft + "px";
-}
-function mvright(){
-    pLeft += step;
-    puck.style.left = pLeft + "px";
-}
+    function mvup(){
+        pTop -= step;
+        puck.style.top = pTop + "px";
+    }
+    function mvdown(){
+        pTop += step;
+        puck.style.top = pTop + "px";
+    }
+    function mvleft(){
+        pLeft -= step;
+        puck.style.left = pLeft + "px";
+    }
+    function mvright(){
+        pLeft += step;
+        puck.style.left = pLeft + "px";
+    }
+
+})();
