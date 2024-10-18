4fa22c77c0eb4eef0f0d0f8e95ead60d9e9d1358
Larry Bouthillier <lbouthillier@post.harvard.edu>
2024-10-17 17:34:27 -0700

    updates for 2024

--- examples/the-global-context-window-bmrux60q/demo.js
+++ examples/the-global-context-window-bmrux60q/demo.js
@@ -1,3 +1,11 @@
+/// NOTE: this code no longer demonstrates what it was supposed to, since
+//    JSFiddle changed their system so that it contains the scope of code
+//    entered by users in the Javascript panel. In the older version of JSFiddle,
+//    users could control page loading behavior (and scope).
+
+// Move this code to local JS files and you will see it behave as documented
+//   and demonstrated in the video.
+
 const school = "Harvard";
 
 console.log(`school is ${school}`);               // Harvard
