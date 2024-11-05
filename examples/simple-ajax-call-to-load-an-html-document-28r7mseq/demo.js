e65afb81c674d272246811559e06124ca9c75804
Larry Bouthillier <lbouthillier@post.harvard.edu>
2024-11-05 12:22:55 -0800

    updates for 2024

--- examples/simple-ajax-call-to-load-an-html-document-28r7mseq/demo.js
+++ examples/simple-ajax-call-to-load-an-html-document-28r7mseq/demo.js
@@ -1,30 +1,17 @@
-var el = document.getElementById("putItHere");
+const el = document.getElementById("output");
 
-//  Create the XHR, intitalize the connection with open()) 
-//    and send the request  
-var xhr = new XMLHttpRequest();
-
-// this one won't work, oddly, due to cross-domain issues
-//xhr.open("GET", "http://jsfiddle.net"); 
-
-xhr.open("GET", "http://fiddle.jshell.net"); 
-
-// Here's an additional URL to try instead of line 10:
-//xhr.open("GET", "http://courses.dce.harvard.edu/~cscie3/ajax.php"); 
-xhr.send();
-
-
-
-// This one works because in the PHP file on the server, 
-//  the 'Access-Control-Allow-Origin' header has been set like so:
-//  <?php header('Access-Control-Allow-Origin: *');?>
-
-
-//  Add a listener function to respond to the HTTP response
-xhr.addEventListener("readystatechange", function(){
-//  Check here for new state and HTTP response code
-//   and write the response to the DIV
-    if(this.readyState == 4 && this.status == 200){
-         el.innerHTML = this.response;   
+fetch("https://learningapi.com/cscie3/ajax.php")
+	.then(response => {
+    if(!response.ok){
+      throw new Error(response.statusText);
     }
-});
+    return response.json();
+  })
+  .then(function(json){
+    console.log(json);
+  })    
+  .catch(error => console.log('error', error));
+
+
+
+
