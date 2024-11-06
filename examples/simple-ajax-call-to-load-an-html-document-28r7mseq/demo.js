30eac1e3f3cf498993b19e96d61035e233a7f5e0
Larry Bouthillier <lbouthillier@post.harvard.edu>
2024-11-05 16:09:00 -0800

    updates for 2024

--- examples/simple-ajax-call-to-load-an-html-document-28r7mseq/demo.js
+++ examples/simple-ajax-call-to-load-an-html-document-28r7mseq/demo.js
@@ -1,17 +1,30 @@
-const el = document.getElementById("output");
 
 fetch("https://learningapi.com/cscie3/ajax.php")
+ // fetch() promise resolves to Response object
 	.then(response => {
     if(!response.ok){
       throw new Error(response.statusText);
     }
     return response.json();
   })
+// response.json() promise resolves to json object
   .then(function(json){
+    console.log(".then() version");
     console.log(json);
   })    
-  .catch(error => console.log('error', error));
+  .catch(error => console.log('error:', error));
 
-
-
+window.onload = async function(){
+	try{
+    // fetch() promise resolves to Response object
+        let response = await fetch("https://learningapi.com/cscie3/ajax.php")
+        if (!response.ok) throw new Error(response.statusText);
+    // response.json() promise resolves to json object
+        let json = await response.json();
+        console.log("async/await version");
+        console.log(json);
+    }catch(error){
+  	    console.log(error);
+    }
+}
 
